45f845997c374ca10e283b95d76994bc8bf594f5
[Mixtrix] <[jefferson024@hotmail.com]>
2026-06-15 17:28:52 -0500

    se agrego una nueva propiedad llamada juego # para la enumeracion de los juegos en la pantalla del usuario

--- src/components/juegos.tsx
+++ src/components/juegos.tsx
@@ -4,6 +4,7 @@ export function Juegos(props: juegos )
 {
     return(
     <div>
+        <h3>Juego # {props.id}</h3>
         <p>Titulo: {props.titulo}</p>
         <p>Genero: {props.genero}</p>
         <p>Plataforma: {props.plataforma}</p>
