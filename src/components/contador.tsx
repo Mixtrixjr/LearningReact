84b8d5cfd2695bcb497b1d51c70c6cae044bf76b
[Mixtrix] <[jefferson024@hotmail.com]>
2026-06-15 17:53:40 -0500

    se agrego estilos dinamicos css a la etiqueta de contador

--- src/components/contador.tsx
+++ src/components/contador.tsx
@@ -5,14 +5,14 @@ export function Contador()
     const [cuenta, setCuenta ] = useState<number>(0)
 
 function manejarcuenta(numero:number) {
-    return setCuenta(cuenta + numero) 
+     setCuenta(cuenta + numero) 
 }
 
     return(
     <div>
         <h3>Contador</h3>
 
-        <p>Usted ha aumentado el contador en {cuenta}</p>
+        <p style={{color:cuenta > 0 ? "green": cuenta < 0 ?"red": "black" }}>Usted ha aumentado el contador en {cuenta}</p>
 
         <button onClick={()=>manejarcuenta(+1)}>Aumentar +1</button>
 
